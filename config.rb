af05e5939230e670bb5b3e132527b9a607318baa
codesenseAye <44715949+codesenseAye@users.noreply.github.com>
2022-03-20 12:07:17 -0500

    Added dependencies and start

---
+++ config.rb
@@ -0,0 +1,14 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "scripts"
+
+output_style = :expanded # or :nested or :compact or :compressed
+
+# relative_assets = true
+# line_comments = false
+
+preferred_syntax = :sass
---
+++ scripts/entry.js
@@ -0,0 +1,34 @@
+import react from "react"
+import reactDOM from "react-dom"
+
+class Topbar extends react.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (null)
+    }
+}
+
+class Contents extends react.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (null)
+    }
+}
+
+class Body extends react.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (null)
+    }
+}
+
+reactDOM.render(<Body/>, document.getElementById("appBody"))
